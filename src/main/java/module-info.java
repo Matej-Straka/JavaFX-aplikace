0b238055b0d358f47aebd718fc0e2d6c8224dccc
Matěj Straka <a@a.com>
2023-12-08 23:40:06 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module test.oauh.cz.javafxaplikace {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+            requires com.dlsc.formsfx;
+                        
+    opens test.oauh.cz.javafxaplikace to javafx.fxml;
+    exports test.oauh.cz.javafxaplikace;
+}
